39e0bb8646cce3a58bc6e589d8908330f5010edb
Knand1861 <renegadecash436@gmail.com>
2025-05-22 05:37:56 +0300

    modified:   data.xlsx 	modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 import os
+import re
 from openpyxl import load_workbook, Workbook
 ITEM = "RTX 3060"
 def main():
+    global ITEM 
+    ITEM = input("Enter item name:")
     items = Items()
+    # Find items on the web
     items.append(fetch_shop("RD Electronics"))
+    items.append(fetch_shop("Euronics"))
+    # Sort by price
     items.sort()
+    # Save and open excel
     save_to_excel(items)
 
 def save_to_excel(items):
@@ -16,16 +24,16 @@ def save_to_excel(items):
         workbook = load_workbook(file_path)
     else:
         workbook = Workbook()
-        # Delete the old sheet
-
+    # Delete old sheets
     for i in workbook.sheetnames:
         workbook.remove(workbook[i])
-    
-    # Add new empty sheet with same name
+    # Add new empty sheet
     workbook.create_sheet("Sheet 1")
     sheet = workbook.active
+
     print("\nOverall item count:",items.length)
 
+    # Build the Excel
     sheet[f"A1"] = "Item name"
     sheet[f"B1"] = "Price"
     sheet[f"C1"] = "Shop"
@@ -35,6 +43,7 @@ def save_to_excel(items):
         sheet[f"B{i+bias}"] = items.get(i).price
         sheet[f"C{i+bias}"] = items.get(i).shop
     workbook.save(file_path)
+    workbook.close() 
 
 
 
@@ -71,6 +80,7 @@ class Items:
 
 def fetch_shop(shop:str):
     items = Items()
+    print("Looking for",ITEM,"in",shop)
     match shop:
         case "RD Electronics":
             URL = "https://www.rdveikals.lv/search/lv/word/"+ITEM+"/page/1/"
@@ -79,10 +89,22 @@ def fetch_shop(shop:str):
             soup:str = BeautifulSoup(html, "html.parser")
             itemContainers = soup.find_all("li", class_="product")
             for i in itemContainers:
-                price = float(i.find("p", class_="price").get_text().replace("€", "").replace(",", ".").strip())
+                price = float(re.sub(r"[:a-zA-Z\s]+","",i.find("p", class_="price").get_text().replace("€", "").replace(",", ".").strip()))
                 name = i.find("div", class_="product__info__part").find("a").get_text()
-                name = ' '.join(name.split())
+                name = " ".join(name.split())
                 items.add(name,price,"RD Electronics")
+        case "Euronics":
+            URL = "https://www.euronics.lv/en/search/"+ITEM
+            response = requests.get(URL)
+            html:str = response.text
+            soup:str = BeautifulSoup(html, "html.parser")
+            itemContainers = soup.find_all("article", class_="product-card")
+            for i in itemContainers:
+                price = i.find("div", class_="price").get_text().split("€")[0]
+                price = float(re.sub(r"[:a-zA-Z\s]+","",price.replace("€", "").replace(",", ".").strip()))
+                name = i.find("a", class_="product_name").get_text()
+                name = " ".join(name.split())
+                items.add(name,price,"Euronics")
     
     print("\nShop:",shop,"\nItem count:",items.length)
     return items
